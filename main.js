d7d3c4b55c057ef4e48a1a2756f4159af1d65f6b
sabrina-fontana <sabfon@hotmail.it>
2021-01-12 15:18:54 +0100

    bonus con hover

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-$('.fa-bars').click(function() {
-  $('.header-right ul').css('display', 'block');
-  $(this).css('display', 'none');
+// $('.fa-bars').click(function() {
+//   $('.header-right ul').css('display', 'block');
+//   $(this).css('display', 'none');
+// });
+
+// BONUS
+$('.header-right > a').hover(function() {
+    $('.header-right ul').css('display', 'block');
+    $('.header-right > a').css('display', 'none');
+});
+
+$('.header-right').mouseleave(function() {
+  $('.header-right ul').css('display', 'none');
+  $('.header-right > a').css('display', 'block');
 });
